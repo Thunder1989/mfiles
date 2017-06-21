885e77c5b8d1442170121ddb38414dc8c4169e6f
Dezhi Hong <dzhong1989@gmail.com>
2017-06-21 18:33:23 -0400

    fixed coefficient for mu and sigma

--- Relationship/gibbs_hmm.m
+++ Relationship/gibbs_hmm.m
@@ -4,7 +4,7 @@ function output = gibbs_hmm(data, debug)
     X2 = [0 diff(X1)];
     Y1 = EWMA(X1,5);
     Y2 = EWMA(X2,5);
-    F = [1 1; 0 1];
+    F = [1 1; 0 2];
     H = [1 0; 0 1];
     
     fprintf('initial Q and R:\n')
@@ -19,7 +19,7 @@ function output = gibbs_hmm(data, debug)
     
     p_data = zeros(K,1);
     for k=1:K
-        fprintf('iter #%d...\n',k);
+        fprintf('--------------iter #%d--------------\n',k);
         %E step
         Y_sample = repmat(Y,1,1,N)*0;
         p_tmp = zeros(N,1);
@@ -30,23 +30,23 @@ function output = gibbs_hmm(data, debug)
 %                 mu = (X(t)*sigma_^2 + mu_*R^2) / (sigma_^2 + R^2);
 %                 sigma = sqrt( Q^2/2 * R^2/(Q^2/2 + R^2) );
 %                 Y_sample(n,t) = normrnd(mu, sigma); %sampling y_t
-                Sigma_ = (Q^-1 + ((F^-1)'*Q*F^-1)^-1)^-1;
-                Mu_ = Sigma_*Q^-1*(F*Y(t-1,:)') + Sigma_*((F^-1)'*Q*F^-1)^-1*(F^-1*Y(t+1,:)');
-                Sigma = (Sigma_^-1 + ((H^-1)'*R*H^-1)^-1)^-1;
-                Mu = Sigma*Sigma_^-1*Mu_ + Sigma*((H^-1)'*R*H^-1)^-1*(H^-1*X(t,:)');
+                Sigma_ = (Q^-1 + (F^-1*Q*(F^-1)')^-1)^-1;
+                Mu_ = Sigma_* Q^-1 * (F*Y(t-1,:)') + Sigma_ * (F^-1*Q*(F^-1)')^-1 * (F^-1*Y(t+1,:)');
+                Sigma = (Sigma_^-1 + (H^-1*R*(H^-1)')^-1)^-1;
+                Mu = Sigma * Sigma_^-1 * Mu_ + Sigma * (H^-1*R*(H^-1)')^-1 * (H^-1*X(t,:)');
                 Y_sample(t,:,n) = mvnrnd(Mu, Sigma);
             end
             
-%             %data likelihood
-%             p = 0;
-%             for t=2:size(Y,2)-1
-%                 p = p + log( normpdf(Y_sample(n,t), Y_sample(n,t-1), Q) )...
-%                     + log( normpdf(X(t), Y_sample(n,t), R) );
-%             end
-%             p_tmp(n) = p;
+            %data likelihood
+            p = 0;
+            for t=2:size(Y,1)-1 
+                p = p + log( mvnpdf(Y_sample(t,:,n), Y_sample(t-1,:,n)*F', Q) )...
+                    + log( mvnpdf(X(t,:), Y_sample(t,:,n)*H', R) );
+            end
+            p_tmp(n) = p;
         end
-        
-%         p_data(k) = mean(p_tmp);
+
+        p_data(k) = mean(p_tmp);
         
         %M step
         Y = mean(Y_sample,3);
@@ -62,7 +62,7 @@ function output = gibbs_hmm(data, debug)
         end
     end
     
-%     figure
-%     plot(p_data, 'k','LineWidth',2)
+    figure
+    plot(p_data, 'k','LineWidth',2)
     
     output = Y;
