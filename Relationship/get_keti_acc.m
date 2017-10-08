7bb07280e52d2355309615c16a75a5ad0a608ec3
Dezhi Hong <dzhong1989@gmail.com>
2017-10-08 19:53:19 -0400

    added keti script based on aaai method

---
+++ Relationship/get_keti_acc.m
@@ -0,0 +1,56 @@
+function get_keti_acc(event, canny)
+
+    path = 'D:\TraneData\KETI_oneweek\';
+    rooms = dir(path);
+
+    ref = 2;
+    ctr = 0;
+    ctr1 = 0;
+    for m = 1:length(event)
+        if mod(m,4)==ref
+            continue
+        end
+        event_cur = event{m};
+        edge_cur = canny{m};
+
+        sims = zeros(length(rooms)-2,1);
+        scores = zeros(length(rooms)-2,1);
+        for n = ref:4:length(event)
+            event_ref = event{n};
+            edge_ref = canny{n};
+
+            if sum(edge_ref)==0 || sum(edge_cur)==0
+                sims(ceil(n/4)) = 0;
+            else
+                [edge_ref, edge_cur] = align_vector(edge_ref, edge_cur);
+                cur_sim = dot(edge_ref, edge_cur)/(norm(edge_ref)*norm(edge_cur));
+                sims(ceil(n/4)) = cur_sim;
+            end
+            
+            if sum(event_ref)==0 || sum(event_cur)==0
+                scores(n) = 0;
+            else
+                [event_ref, event_cur] = align_vector(event_ref, event_cur);
+                scores(n) = dot(event_ref, event_cur) / ( norm(event_ref) * norm(event_cur) );
+            end
+        end
+
+        true = ceil(m/4);
+        if ismember(true, find(sims==max(sims))) && length( find(sims==max(sims)) ) < length(sims);
+            ctr = ctr + 1;
+        else
+            flag = ismember( true, find(scores==max(scores)) ) & length( find(scores==max(scores)) ) < length(scores);
+            if flag == 1
+                ctr1 = ctr1 + 1;
+            end
+        end
+    end
+
+    fprintf('acc on canny edge is %.4f\n', ctr/((length(rooms)-2)*3));
+    fprintf('acc on combination is %.4f\n', (ctr+ctr1)/((length(rooms)-2)*3));
+
+function [a_, b_] = align_vector(a,b)
+    len = min(length(a), length(b));
+    a_ = a(1:len);
+    b_ = b(1:len);
+    
---
+++ Relationship/get_keti_events.m
@@ -0,0 +1,43 @@
+function [event, canny] = get_keti_events()
+    close all
+    clc
+
+    path = 'D:\TraneData\KETI_oneweek\';
+    rooms = dir(path);
+
+    event = cell((length(rooms)-2)*4, 1);
+    canny = cell((length(rooms)-2)*4, 1);
+    for n = 3:length(rooms)
+        fprintf('processing %s ...\n',rooms(n).name);
+        files = dir(strcat(path, rooms(n).name, '\*.csv'));
+        ctr = 1;
+        for m = 1:length(files)
+            if m==4
+                continue %skip pir for now
+            end
+            fn = strcat(path, rooms(n).name, '\', files(m).name);
+            cur_data = csvread(fn,1); %skip the 1st row, which is the headers
+            cur_data = cur_data(:,2);
+            cur_data = cur_data(1:30:end); %downsample
+%             plot(cur_data)
+%             pause
+    %         [res,Z,M] = gibbs_sgf(cur_ahu,1,0);
+            event{(n-3)*4+ctr} = get_KF_residue(cur_data');
+            canny{(n-3)*4+ctr} = get_canny_edges(cur_data');
+            ctr = ctr + 1;
+        end
+    end
+
+function res = get_KF_residue(X) %X is a row input vector
+    N = 5*2; %Kalman Filter lookback window size
+    M = 8; %EWMA window size
+    kf = StandardKalmanFilter(X,M,N,'EWMA'); 
+    res = abs(X - kf);
+    res(isnan(res)) = 0;
+    
+function event = get_canny_edges(X) %X is a row input vector
+    th = 1.25;    
+    e = edge(repmat(X,3,1), th);
+    e = e(1,:);
+    e = e | [false e(1:end-1)] | [e(2:end) false];
+    event = double(e);
